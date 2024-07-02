be706940010b44985f16d725ebcdde1258372772
portable <emmanueltemitopedorcas20@gmail.com>
2024-07-02 01:42:04 +0100

    remove console and vscode

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function updateTime() {
     const currentTimeUTC = new Date().toUTCString();
-    console.log(currentTimeUTC)
+    // console.log(currentTimeUTC)
     document.getElementById('currentTimeUTC').textContent = currentTimeUTC.split(' ')[4];
     document.getElementById('currentDay').textContent = currentTimeUTC.split(',')[0];
 }
